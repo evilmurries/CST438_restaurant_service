af5741fb079df897e594efc607fd12ce8a87c74f
xpy <cpiwarski01@gmail.com>
2020-04-04 12:47:27 -0700

    initial commit

---
+++ src/main/java/com/example/demo/Cst438RestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438RestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438RestaurantServiceApplication.class, args);
+	}
+
+}
