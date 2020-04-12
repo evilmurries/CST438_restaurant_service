eb672329aaffb866aefe0cc26b6a0733b9b26098
xpy <cpiwarski01@gmail.com>
2020-04-12 10:14:46 -0700

    wip

--- src/main/java/com/example/demo/RestaurantController.java
+++ src/main/java/com/example/demo/RestaurantController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,7 @@ public class RestaurantController
    RestaurantService restaurantService;
    
    
-   @GetMapping("/restaurant/name/{name}")
-   @ResponseBody
+   @RequestMapping("/restaurant/name/{name}")
    public ResponseEntity<Restaurant> findRestaurantById(@PathVariable("name") String name) {
       Restaurant restaurant = restaurantService.getRestaurantName(name);
       if (restaurant == null) {
